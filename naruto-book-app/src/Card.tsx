bf8ad1cdf27ee735cbb966bb685417c73410a293
nakanoyuki <yukich41@gmail.com>
2025-01-05 19:44:00 +0900

    表示する情報追加

--- naruto-book-app/src/Card.tsx
+++ naruto-book-app/src/Card.tsx
@@ -1,18 +1,28 @@
 import { FC } from "react";
 import "./App.css";
+import { Character } from "./App";
 
 type Props = {
-  images: string[];
+  character: Character;
 };
 
-export const Card: FC<Props> = ({ images }) => {
+export const Card: FC<Props> = ({ character }) => {
   return (
     <div className="card">
       <img
-        src={images[0] || "/dummy.png"}
+        src={character.images[0] || "/dummy.png"}
         alt="Character"
         className="card-image"
       />
+      <div className="card-content">
+        <h3 className="card-title">{character.name}</h3>
+        <p className="card-scription">{character.debut?.appersIn ?? "なし"}</p>
+        <div className="card-footer">
+          <div className="affiliation">
+            {character.personal?.affiliation ?? "なし"}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
