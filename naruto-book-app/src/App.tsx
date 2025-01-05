03b332db3b7b38b0caa4edd8008d7a35782bfd90
nakanoyuki <yukich41@gmail.com>
2025-01-05 18:31:22 +0900

    データを非同期で取得

--- naruto-book-app/src/App.tsx
+++ naruto-book-app/src/App.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+import { Card } from "./Card";
+
+type Character = {
+  id: number;
+  name: string;
+  images: string[];
+};
 
 function App() {
-  return <div className="App">Hello</div>;
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  const fetchCharacters = async () => {
+    const apiUrl = "https://narutodb.xyz/api/character";
+
+    const result = await axios.get(apiUrl);
+    setCharacters(result.data.characters);
+  };
+
+  return (
+    <div className="container">
+      <main>
+        <div className="cards-container">
+          {characters.map((character) => (
+            <Card
+              key={character.id}
+              images={character.images}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
