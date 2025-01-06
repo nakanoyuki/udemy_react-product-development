b71a01f236156f04ead3ced73524948a91389117
nakanoyuki <yukich41@gmail.com>
2025-01-06 22:40:15 +0900

    loading、nextPagenationの実装

--- naruto-book-app/src/App.tsx
+++ naruto-book-app/src/App.tsx
@@ -13,27 +13,47 @@ export type Character = {
 
 function App() {
   const [characters, setCharacters] = useState<Character[]>([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    fetchCharacters();
+    fetchCharacters(page);
   }, []);
 
-  const fetchCharacters = async () => {
+  const fetchCharacters = async (page: number) => {
     const apiUrl = "https://narutodb.xyz/api/character";
-
-    const result = await axios.get(apiUrl);
+    setLoading(true);
+    const result = await axios.get(apiUrl, { params: { page } });
     setCharacters(result.data.characters);
+    setLoading(false);
+  };
+
+  const handleNext = async () => {
+    const nextPage = page + 1;
+    await fetchCharacters(nextPage);
+    setPage(nextPage);
   };
 
   return (
     <div className="container">
-      <main>
-        <div className="cards-container">
-          {characters.map((character) => (
-            <Card key={character.id} character={character} />
-          ))}
-        </div>
-      </main>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <main>
+          <div className="cards-container">
+            {characters.map((character) => (
+              <Card key={character.id} character={character} />
+            ))}
+          </div>
+          <div className="pager">
+            <button className="prev">Prev</button>
+            <span className="page-number">{page}</span>
+            <button className="next" onClick={handleNext}>
+              Next
+            </button>
+          </div>
+        </main>
+      )}
     </div>
   );
 }
