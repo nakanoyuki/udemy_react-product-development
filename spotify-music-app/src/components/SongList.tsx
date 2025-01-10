cfcf6358f12b43216c4dc7deb8b6b80eee8f52ac
nakanoyuki <yukich41@gmail.com>
2025-01-11 00:06:19 +0900

    propsでデータを渡す

--- spotify-music-app/src/components/SongList.tsx
+++ spotify-music-app/src/components/SongList.tsx
@@ -1,26 +1,27 @@
-export function SongList() {
+export function SongList({ songs }: any) {
   return (
     <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
-      {/* {Array(20).map(() => {
-        return ( */}
-      <a
-        href="https://open.spotify.com/intl-ja/track/78W4mTLIh4qoLu92W4IQhO"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex-none cursor-pointer "
-      >
-        <img
-          alt="thumbnail"
-          src={
-            "https://i.scdn.co/image/ab67616d0000b2738b7a8c1322028d45a8355f7a"
-          }
-          className="mb-2 rounded"
-        />
-        <h3 className="text-lg font-semibold">Song Name</h3>
-        <p className="text-gray-400">By Artist</p>
-      </a>
-      {/* );
-      })} */}
+      {songs.map(() => {
+        console.log(songs);
+        return (
+          <a
+            href="https://open.spotify.com/intl-ja/track/78W4mTLIh4qoLu92W4IQhO"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-none cursor-pointer "
+          >
+            <img
+              alt="thumbnail"
+              src={
+                "https://i.scdn.co/image/ab67616d0000b2738b7a8c1322028d45a8355f7a"
+              }
+              className="mb-2 rounded"
+            />
+            <h3 className="text-lg font-semibold">Song Name</h3>
+            <p className="text-gray-400">By Artist</p>
+          </a>
+        );
+      })}
     </div>
   );
 }
