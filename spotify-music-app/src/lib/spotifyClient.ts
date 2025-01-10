1c4916538b875fd130b9b31e97f6e19cffcc80f1
nakanoyuki <yukich41@gmail.com>
2025-01-10 23:25:33 +0900

    人気の曲を取得する関数の追加

--- spotify-music-app/src/lib/spotifyClient.ts
+++ spotify-music-app/src/lib/spotifyClient.ts
@@ -1,15 +1,25 @@
 import { getToken } from "./getToken";
+import axios from "axios";
 
-class ShopifyClient {
+class SpotifyClient {
   token: string | null = null;
 
   static async initialize() {
     const getTokenData = await getToken();
 
-    const spotify = new ShopifyClient();
+    const spotify = new SpotifyClient();
     spotify.token = getTokenData.access_token;
     return spotify;
   }
+
+  async getPopularSongs() {
+    const response = await axios.get(
+      "https://api.spotify.com/v1/playlists/5SLPaOxQyJ8Ne9zpmTOvSe",
+      { headers: { Authorization: "Bearer " + this.token } }
+    );
+    console.log(response.data);
+    return response.data;
+  }
 }
 
-export default ShopifyClient;
+export default SpotifyClient;
