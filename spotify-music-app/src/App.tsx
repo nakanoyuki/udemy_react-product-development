323e17ae907952ee0c10d550c50e2fb4c750964b
nakanoyuki <yukich41@gmail.com>
2025-01-13 21:18:51 +0900

    handleInputChange関数追加しSearchInputに渡す

--- spotify-music-app/src/App.tsx
+++ spotify-music-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import SpotifyClient from "./lib/spotifyClient";
 import { SongList } from "./components/SongList";
+import { SearchInput } from "./components/SearchInput";
 
 export const processSongsData = (items: any) => {
   return items.map((item: any) => ({
@@ -10,6 +11,8 @@ export const processSongsData = (items: any) => {
 
 export default function App() {
   const [popularSongs, setPopularSongs] = useState([]);
+  const [keyword, setKeyWord] = useState("");
+  const [searchedSongs, setSearchedSongs] = useState();
   const [, setSpotify] = useState<SpotifyClient | null>(null);
   const [loading, setLoading] = useState(false);
 
@@ -26,14 +29,18 @@ export default function App() {
     };
     initializeSpotify();
   }, []);
+  
 
-  // console.log(popularSongs);
+  const handleInputChange = (e: any) => {
+    setSearchedSongs(e.target.value);
+  };
   return (
     <div className="flex flex-col min-h-screen bg-gray-900 text-white">
       <main className="flex-1 p-8 mb-20">
         <header className="flex justify-between items-center mb-10">
           <h1 className="text-4xl font-bold">Music App</h1>
         </header>
+        <SearchInput handleInputChange={handleInputChange} />
         <section>
           <h2 className="text-2xl font-semibold mb-5">Popular Songs</h2>
           <SongList loading={loading} songs={popularSongs} />
