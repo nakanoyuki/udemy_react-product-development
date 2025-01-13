e04b9ea31bf3288819508a66ac15c9eb5f7a599a
nakanoyuki <yukich41@gmail.com>
2025-01-13 21:18:28 +0900

    Spotify API を使って楽曲を検索する関数をgetSearchSongs作成

--- spotify-music-app/src/lib/spotifyClient.ts
+++ spotify-music-app/src/lib/spotifyClient.ts
@@ -19,6 +19,14 @@ class SpotifyClient {
     );
     return response.data.tracks.items;
   }
+
+  async getSearchSongs(keyword: any) {
+    const response = await axios.get("https://api.spotify.com/v1/search", {
+      headers: { Authorization: "Bearer " + this.token },
+      params: { q: keyword, type: "track" },
+    });
+    return response.data.tracks.items;
+  }
 }
 
 export default SpotifyClient;
