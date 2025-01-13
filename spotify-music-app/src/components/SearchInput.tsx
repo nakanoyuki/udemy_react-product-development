69d7b90414a42d99a1d937ffc0c04f1f433502b9
nakanoyuki <yukich41@gmail.com>
2025-01-13 21:19:03 +0900

    onChangeイベント追加

--- spotify-music-app/src/components/SearchInput.tsx
+++ spotify-music-app/src/components/SearchInput.tsx
@@ -1,10 +1,11 @@
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export function SearchInput() {
+export function SearchInput(handleInputChange: any) {
   return (
     <section className="mb-10">
       <input
+        onChange={handleInputChange}
         className="bg-gray-700 w-1/3 p-2 rounded-l-lg focus:outline-none"
         placeholder="探したい曲を入力してください"
       />
