d375971f688ef65d200fdb0370b395fcbd91bb58
nakanoyuki <yukich41@gmail.com>
2025-01-19 23:17:27 +0900

    ルーティングの設定

--- sns-app/src/App.tsx
+++ sns-app/src/App.tsx
@@ -1,10 +1,19 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <div>
-      <header>uu</header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
