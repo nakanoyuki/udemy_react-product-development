65257b10937e5f65ebe62884e5c64b1deebaf3e9
nakanoyuki <yukich41@gmail.com>
2025-01-22 00:58:45 +0900

    typo

--- sns-app/src/lib/supabase.ts
+++ sns-app/src/lib/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUBBASE_URL;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseApiKey = process.env.REACT_APP_SUPABASE_APIKEY;
 
 if (!supabaseUrl || !supabaseApiKey) {
   throw new Error(
-    "Missing Supabase environment variables: REACT_APP_SUBBASE_URL or REACT_APP_SUPABASE_APIKEY"
+    "Missing Supabase environment variables: REACT_APP_SUPABASE_URL or REACT_APP_SUPABASE_APIKEY"
   );
 }
 
