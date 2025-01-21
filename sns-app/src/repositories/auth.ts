09cc4fc20aa78b8adaf868f47cee975367305da5
nakanoyuki <yukich41@gmail.com>
2025-01-21 22:46:57 +0900

    authRepository関数でユーザー登録機能の作成

---
+++ sns-app/src/repositories/auth.ts
@@ -0,0 +1,13 @@
+import { supabase } from "../lib/supabase";
+
+export const authRepository = {
+  async signup(name: any, email: any, password: any) {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: { data: { name } },
+    });
+    if (error != null) throw new Error(error.message);
+    return { ...data.user, userName: data.user?.user_metadata?.name };
+  },
+};
