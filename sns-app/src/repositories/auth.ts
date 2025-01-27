7d991eaaf143741594e82a628e9dacf9ba045a26
nakanoyuki <yukich41@gmail.com>
2025-01-27 23:28:12 +0900

    getCurrentUser関数の追加

--- sns-app/src/repositories/auth.ts
+++ sns-app/src/repositories/auth.ts
@@ -19,4 +19,15 @@ export const authRepository = {
     if (error) throw new Error(error.message);
     return { ...data.user, userName: data.user?.user_metadata?.name };
   },
+
+  async getCurrentUser() {
+    const { data, error } = await supabase.auth.getSession();
+    if (error != null) throw new Error(error.message);
+    if (data.session == null) return;
+
+    return {
+      ...data.session.user,
+      userName: data.session.user?.user_metadata?.name,
+    };
+  },
 };
